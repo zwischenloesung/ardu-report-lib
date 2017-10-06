b48e976dedb145c212eab2a7d3a97bc9f11c088e
Michael Lustenberger <mic@inofix.ch>
2017-10-06 22:57:26 +0200

    Version Info
    Start with a clean base.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ardu-report',
-    version='0.9',
+    version='0.1.dev11+g0385766',
     description='Get sensor data over the serial line and send it to an URL (JSON).',
     long_description=long_description,
 
