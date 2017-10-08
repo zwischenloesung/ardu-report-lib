69bf4ae0746b4540b1419a5383f8244174d6eed0
Michael Lustenberger <mic@inofix.ch>
2017-10-08 23:27:09 +0200

    Exception Syntax Error
    Make also python3 happy, unfortunately I only run the python2 test
    suite.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ardu-report-lib',
-    version='1.0.1.dev3+ga745b12',
+    version='1.0.1.dev4+g88e86dd',
     description='Get sensor data over the serial line and send it to an URL (JSON).',
     long_description=long_description,
 
